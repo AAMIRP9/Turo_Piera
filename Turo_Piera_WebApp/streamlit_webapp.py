57a1d9d12181e2b66c2c432ecefe7516e16f6cac
Aamir Pirani <91920656+AAMIRP9@users.noreply.github.com>
2024-10-25 17:08:11 +0200

    Update streamlit_webapp.py

--- Turo_Piera_WebApp/streamlit_webapp.py
+++ Turo_Piera_WebApp/streamlit_webapp.py
@@ -10,73 +10,73 @@ import matplotlib.offsetbox as offsetbox
 # --- PAGE SETUP ---
 
 home = st.Page(
-    page="Home.py",
+    page="Pages/Home.py",
     title="Home", 
     icon=":material/home:",
     default=True,
 )
 
 match_stats = st.Page(
-    page="Match_Stats.py",
+    page="Pages/Match_Stats.py",
     title="Match Stats",
     icon=":material/query_stats:",
 )
 
-Possession = st.Page(
-    page="Possession.py",
+possession = st.Page(
+    page="Pages/Possession.py",
     title="Possession",
     icon=":material/sports_soccer:",
 )
 
 goals_and_xg = st.Page(
-    page="Goals_&_xG.py",
+    page="Pages/Goals_&_xG.py",
     title="Goals & xG",
     icon=":material/sports_soccer:",
 )
 
 shooting = st.Page(
-    page="Shooting.py",
+    page="Pages/Shooting.py",
     title="Shooting",
     icon=":material/sports_soccer:",
 )
 
 tackles = st.Page(
-    page="Tackles.py",
+    page="Pages/Tackles.py",
     title="Tackles",
     icon=":material/sports_soccer:",
 )
 
 clearances = st.Page(
-    page="Clearances.py",
+    page="Pages/Clearances.py",
     title="Clearances",
     icon=":material/sports_soccer:",
 )
 
 freekicks = st.Page(
-    page="Freekicks.py",
+    page="Pages/Freekicks.py",
     title="Freekicks",
     icon=":material/sports_soccer:",
 )
 
 fouls = st.Page(
-    page="Fouls.py",
+    page="Pages/Fouls.py",
     title="Fouls",
     icon=":material/sports_soccer:",
 )
 
 saves = st.Page(
-    page="Saves.py",
+    page="Pages/Saves.py",
     title="Saves",
     icon=":material/sports_soccer:",
 )
 
 offsides = st.Page(
-    page="Offsides.py",
+    page="Pages/Offsides.py",
     title="Offsides",
     icon=":material/sports_soccer:",
 )
 
 # --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
-pg = st.navigation(pages=[home, match_stats, Possession, goals_and_xg, shooting, tackles, clearances, fouls, freekicks, saves, offsides])
+pg = st.navigation(pages=[home, match_stats, possession, goals_and_xg, shooting, tackles, clearances, fouls, freekicks, saves, offsides])
 
 # 
